155eb680805562052e98bd1131eec4b00b4fa1ec
[vanshgusain] <vanshgusain18@gmail.com>
2024-09-08 17:43:04 +0530

    learned about admin page

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Products, Offer
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display=("name", "stock", "price")
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display=("code", "description", "discount")
+
+admin.site.register(Products, ProductAdmin)
+admin.site.register(Offer, OfferAdmin)
